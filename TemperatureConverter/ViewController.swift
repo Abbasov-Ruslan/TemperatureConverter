34c43685f1ff09e1ca2bfa859338dd17f7d907b3
Ruslan Abasov <abbasov_ruslan1999@mail.ru>
2019-10-08 11:41:03 +0700

    Made code part with converting celsius in fahrenjeits, and slider

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider!) {
+        let celsiusTemperature = Int(round(sender.value))
+        celsiusLabel.text = "\(celsiusTemperature)°C"
+        
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(fahrenheitTemperature)°F"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
